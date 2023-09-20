6309c8da5adb8ea62eaa5a2f58a0773b20fa6bef
Rogelio Yair Reyes Zurita <reyesryz2002@gmail.com>
2023-09-20 13:38:29 -0600

    Query params

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ app.get('/:user/:password',(req,res)=>{
     res.status(404).json({msg: 'Error en el usuario o contraseña'})
 })
 
+//  http://localhost:3000/login?user=julio&&password=123
+app.get('/login',(req,res)=>{  //Endpoint
+    const {user,password} = req.query
+    if(!user || !password){
+        res.status(400).json({
+            msg:'You need to provide <user> and <password> parameters'
+        })
+        return
+
+    }
+
+
+    if(user=== 'Rogelio' && password ==='123'){
+        res.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+
+
+    res.status(404).json({msg: 'Error en el usuario o contraseña'})
+
+
+})
 
 
 
