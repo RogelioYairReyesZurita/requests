36144bc29562658942884ca18b3474db19719c00
Rogelio Yair Reyes Zurita <reyesryz2002@gmail.com>
2023-10-02 13:36:38 -0600

    request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get('/:user/:password',(req,res)=>{
 
@@ -43,9 +44,28 @@ app.get('/login',(req,res)=>{  //Endpoint
 
 
 
-app.post('/',(request,response)=>{
-    response.json({msg:'Hola Post'})
+app.post('/login',(req,res)=>{
+    const {user,password} = req.body
+    if(!user || !password){
+        res.status(400).json({
+            msg:'You need to provide <user> and <password> parameters'
+        })
+        return
+
+    }
+
+
+    if(user=== 'Rogelio' && password ==='123'){
+        res.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+
+
+    res.status(404).json({msg: 'Error en el usuario o contraseña'})
+
 })
+
+
 app.put('/',(request,response)=>{
     response.jsom({msg:'Hola Put'})
 })
